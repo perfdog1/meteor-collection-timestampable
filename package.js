e9752a6d8fbfc0b61337bb87270a5cd1043f863f
Rod Cope <rod@perfdog.com>
2024-07-20 13:38:02 -0600

    More hacks again again again

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   git: 'https://github.com/rodcope1/meteor-collection-timestampable.git',
   name: 'rodcope1:collection-timestampable',
   summary: 'Add timestamps to collections',
-  version: '3.1.4'
+  version: '3.1.5'
 });
 
 Package.onUse(function(api) {
@@ -10,18 +10,18 @@ Package.onUse(function(api) {
 
   api.use([
     'check',
-    'coffeescript@2.7.0',
+    'coffeescript@2.0.0',
     'underscore'
   ]);
 
   api.use([
-    'matb33:collection-hooks@1.3.3',
+    'matb33:collection-hooks@1.3.0',
     'rodcope1:collection-behaviours@3.0.0'
   ]);
 
   api.use([
-    'aldeed:collection2@4.0.3',
-    'aldeed:simple-schema@1.13.1'
+    'aldeed:collection2@4.0.0',
+    'aldeed:simple-schema@1.13.0'
   ], ['client', 'server'], {weak: true});
 
   api.imply('rodcope1:collection-behaviours');
